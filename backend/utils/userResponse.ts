0a6a7d8bf5ddc3814fd1d7d542597339cb589bcc
Sathya Kumar <bvsrakeshkumar@gmail.com>
2026-05-20 21:51:11 +0530

    Update userResponse.ts

--- backend/utils/userResponse.ts
+++ backend/utils/userResponse.ts
@@ -1,11 +1,22 @@
 import type { UserProps, UserResponseProps } from "../types.js";
 
 export const formatUserResponse = (user: UserProps): UserResponseProps => {
-  return {
+  const response: UserResponseProps = {
     _id: user._id,
     email: user.email,
-    name: user.name,
-    avatar: user.avatar,
-    created: user.created,
   };
+
+  if (user.name !== undefined) {
+    response.name = user.name;
+  }
+
+  if (user.avatar !== undefined) {
+    response.avatar = user.avatar;
+  }
+
+  if (user.created !== undefined) {
+    response.created = user.created;
+  }
+
+  return response;
 };
